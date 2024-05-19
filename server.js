8c4bb06e2d4de786677cfaf7cc42c15b6ed11f13
Svitlana-Marchenko <sveta25022004@gmail.com>
2024-05-19 14:49:05 +0300

    feature: add logger

--- server.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-
-const express = require('express');
-const app = express();
-const port = 8000;
-
-app.listen(port, () => console.log('SERVER IS RUNNING ON PORT ' + port));
-
---
+++ src/server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+require('./helpers/emailService');
+
+const rateRoute = require("./routes/rateRoutes")
+const userRoute = require("./routes/userRoutes")
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.listen(PORT, () => console.log('SERVER IS RUNNING ON PORT ' + PORT));
+
+app.use("/api/rate", rateRoute)
+app.use("/api/subscribe", userRoute)
