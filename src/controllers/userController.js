57bf86080af8e17ef84ef690a8ade056a88356e8
Svitlana-Marchenko <sveta25022004@gmail.com>
2024-05-19 15:06:08 +0300

    feature: add user routes

---
+++ src/controllers/userController.js
@@ -0,0 +1,29 @@
+const userService = require('../services/userService');
+const logger = require('../helpers/logger');
+
+const createUser = async (req, res) => {
+    const { email } = req.body;
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        logger.error(`Invalid email ${email} address`)
+        return res.status(400).json({ message: `Invalid email ${email} address` });
+    }
+
+    try {
+        const result = await userService.createUser(email);
+        if (result) {
+            logger.info(`Email ${email} was added to the database`);
+            res.status(200).json({ message: `New email was added to the database` });
+        } else {
+            logger.error(`Email ${email} is already in the database`);
+            res.status(409).json({ message: `Email is already in the database` });
+        }
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+module.exports = {
+    createUser
+};
---
+++ src/services/userService.js
@@ -0,0 +1,29 @@
+const { User } = require('../../db/models');
+
+const createUser = async (email) => {
+    try {
+        const user = await User.findOne({ where: { email: email } });
+        if (user) {
+            return false;
+        }
+        await User.create({ email });
+        return true;
+    } catch (error) {
+        throw new Error('Error creating user: ' + error.message);
+    }
+};
+
+const getAllEmails = async () => {
+    try {
+        const user = await User.findAll();
+        console.log(user)
+        return user.map(u => u.email);
+    } catch (error) {
+        throw new Error('Error getting all users email: ' + error.message);
+    }
+}
+
+module.exports = {
+    createUser,
+    getAllEmails
+};
